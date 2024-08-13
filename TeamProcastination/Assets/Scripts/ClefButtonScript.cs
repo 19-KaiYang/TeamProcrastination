cd420b14412a7ef010bbb5ca5887e6ddd04a5414
13-Clef <114891810+13-Clef@users.noreply.github.com>
2024-08-13 16:57:20 +0800

    shop ui mechanics
    in progress i think

--- TeamProcastination/Assets/Scripts/ClefButtonScript.cs
+++ TeamProcastination/Assets/Scripts/ClefButtonScript.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClefButtonScript : MonoBehaviour
 {
+    public GameObject quantityText;
+    public GameObject yourBroke;
+    private float itemQuantity;
+    public float wallet = 0f; 
+
+    public void OnAddClick()
+    {
+        itemQuantity++;
+    }
+
+    public void OnSubtractClick()
+    {
+        if (itemQuantity <= 0f)
+        {
+            itemQuantity = 0f;
+        }
+        else
+        {
+            itemQuantity--;
+        }
+
+        
+    }
+
+    public void OnPurchaseClick()
+    {
+        if (wallet <= 0f)
+        {
+            yourBroke.SetActive(true);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemQuantity = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
+        
         
     }
 }
